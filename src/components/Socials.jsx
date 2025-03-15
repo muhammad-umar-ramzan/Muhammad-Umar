fd061ff3011975cc024b5d6d9b7a67f79c6de9a7
muhammad-umar-ramzan <ramzan87767@gmail.com>
2025-03-15 06:14:02 +0500

    update socials

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,22 +1,34 @@
 import { socials } from '../constants';
 import { SectionWrapper } from '../hoc';
-import { SphereCanvas } from './canvas';
+import { motion } from 'framer-motion';
 
 const Socials = () => {
-	return (
-		<div className='flex flex-row flex-wrap justify-center max-w-[360px] mx-auto gap-6 -mt-0'>
-			{socials.map(social => (
-				<a
-					className='xs:w-24 w-[70px] xs:h-24 h-[70px]'
-					key={social.name}
-					href={social.link}
-					target='_blank'
-				>
-					<SphereCanvas icon={social.icon} />
-				</a>
-			))}
-		</div>
-	);
+  return (
+    <div className='flex flex-row flex-wrap justify-center max-w-[360px] mx-auto gap-4'>
+      {socials.map((social, index) => (
+        <motion.a
+          className='w-[60px] h-[60px] md:w-[80px] md:h-[80px] flex items-center justify-center bg-white rounded-full shadow-md overflow-hidden'
+          key={social.name}
+          href={social.link}
+          target='_blank'
+          initial={{ x: 0, y: 0 }}
+          animate={{ x: [0, Math.random() * 8 - 4, 0], y: [0, Math.random() * 8 - 4, 0] }}
+          transition={{
+            duration: 3 + index * 0.2,
+            repeat: Infinity,
+            ease: 'easeInOut',
+          }}
+          whileHover={{ scale: 1.1, boxShadow: "0px 0px 10px rgba(255,255,255,0.8)" }}
+        >
+          <img 
+            src={social.icon} 
+            alt={social.name} 
+            className='w-3/5 h-3/5 object-contain'
+          />
+        </motion.a>
+      ))}
+    </div>
+  );
 };
 
 export default SectionWrapper(Socials, 'socials');
