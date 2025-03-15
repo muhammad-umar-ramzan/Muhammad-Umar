dd88cc8ec3d2094b5e81a41828683243dd36bd1d
muhammad-umar-ramzan <ramzan87767@gmail.com>
2025-03-15 06:29:39 +0500

    update Earth.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,41 +1,41 @@
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import CanvasLoader from '../Loader';
 
-const Ball = props => {
-	const [decal] = useTexture([props.imgUrl]);
-
-	return (
-		<Float speed={2} rotationIntensity={1.5} floatIntensity={3}>
-			<ambientLight intensity={0.5} />
-			<directionalLight position={[0, 0, 0.07]} />
-			<mesh castShadow receiveShadow scale={2.7}>
-				<icosahedronGeometry args={[1, 1]} />
-				<meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
-				<Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.26]} flatShading map={decal} />
-			</mesh>
-		</Float>
-	);
+const Earth = () => {
+  const earth = useGLTF('/planet/scene.glb'); // GLB use karo (faster & lightweight)
+  return <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />;
 };
 
-const BallCanvas = ({ icon }) => {
-	return (
-		<Canvas dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
-			<Suspense fallback={<CanvasLoader />}>
-				<OrbitControls
-					enableZoom={false}
-					enablePan={false}
-					maxPolarAngle={Math.PI / 1.6}
-					minPolarAngle={Math.PI / 2.8}
-					maxAzimuthAngle={Math.PI * 2.3}
-					minAzimuthAngle={Math.PI * 1.7}
-					rotateSpeed={0.2}
-				/>
-				<Ball imgUrl={icon} />
-			</Suspense>
-			<Preload all />
-		</Canvas>
-	);
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand" // Optimize rendering (better performance)
+      shadows
+      dpr={[1, 1.5]} // Adjust resolution for better performance
+      camera={{
+        fov: 35,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          autoRotateSpeed={0.5} // Slow rotation to avoid high GPU usage
+          enableZoom={false}
+          enableDamping={true} // Smooth rotation
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
 };
-export default BallCanvas;
+
+export default EarthCanvas;
