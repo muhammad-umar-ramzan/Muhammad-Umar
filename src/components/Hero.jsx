f0b52b8524a320a4a25934caea5eb44edf75b978
muhammad-umar-ramzan <ramzan87767@gmail.com>
2025-03-22 10:15:51 +0500

    update Heros.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
                     <div className='w-[35px] h-[64px] rounded-3xl border-4 border-primary flex justify-center items-start p-2'>
                         <motion.div
                             animate={{ y: [0, 24, 0] }}
-                            transition={{ duration: 30, repeat: Infinity, repeatType: 'loop' }}
+                            transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
                             className='w-3 h-3 rounded-full bg-primary mb-1'
                         />
                     </div>
